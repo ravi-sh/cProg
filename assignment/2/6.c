bd10a2770215e48e59bdfd3338de20437c13914a
ravindra <ravindrarshinde212@gmail.com>
2021-08-09 12:25:12 +0530

    sorting 2d array (based on length)

--- assignment/2/6.c
+++ assignment/2/6.c
@@ -24,26 +24,32 @@ int maxlen(char (*ptr)[MAX_STRLEN], int ele, int len_lim)
     return indx;
 }
 
-void sort2dArr(char (*ptr)[MAX_STRLEN], ele){
+void sort2dArr(char (*ptr)[MAX_STRLEN], int ele){
     int i, j;
     char temp[MAX_STRLEN];
+
     for( i = 0; i < ele-1; i++){
         for(j=i+1; j<ele; j++){
-	    if(strcmp(ptr[i], ptr[j]) > 0){
+	    if(strlen(ptr[i]) < strlen(ptr[j])){
 	        strcpy(temp,ptr[i]);
 	        strcpy(ptr[i],ptr[j]);
 	        strcpy(ptr[j],temp);
 	    }
 	}
     }
-
 }
 
 int main(){
-    char ptr[4][MAX_STRLEN] = { "ravi", "shwetu", "best", "friends" };
+    char ptr[4][MAX_STRLEN] = {"ravi","shwetu","best","friends"};
 
     printf("Max Len String: %s\n", ptr[maxlen(ptr, 4, 4)]);
     printf("Number of Strings which cross the Specified Len: %d\n", count);
+
+    sort2dArr(ptr, 4);
+
+    for(int i =0; i < 4; i++) {
+        printf("%s\n", ptr[i]);
+    }
 
     return 0;
 }
