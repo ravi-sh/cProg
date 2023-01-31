62c7091e7deab2e697182ce458b95b132f2efcd2
ravindra <ravindra.shinde@airfi.aero>
2023-01-31 22:55:10 +0530

    ptr inc/dec/precedence/priority

--- 2-pointers/6-precedence.c
+++ 2-pointers/6-precedence.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
 int main(){
+	int ar[] = {10,20,30,40};
+	int *ptr = ar;
+	
+	//*ptr++;
+	//printf("%d\n", *ptr);
 
+	/* POST INCREAMENTS */
+
+	//printf("%d\n", *ptr++);
+	//printf("%d\n", *ptr);
+	
+	//printf("%d-%d\n", *ptr, *ptr++);
+
+	//printf("%d:%d\n", *ptr++, *ptr);
+	//printf("%d\n", *ptr);
+
+	printf("%d\n", ++*ptr);
+	printf("%d\n", *++ptr);
+	printf("%d\n", ++*ptr);
 
 	return 0;
 }
