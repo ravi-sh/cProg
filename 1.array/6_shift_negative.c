5c3daf05c2e6809935dc24ce417e73f3e416ab7b
ravindra shinde <ravindrarshinde212@gmail.com>
2025-02-09 10:33:21 -0800

    Algorithm to Shift the -ve ele of array to left
    Signed-off-by: ravindra shinde <ravindrarshinde212@gmail.com>

---
+++ 1.array/6_shift_negative.c
@@ -0,0 +1,56 @@
+/********************************
+ * 1. Inserting in sorted array (Always start from last element)
+ * 2. Check if array is sorted
+ * 3. Arranging -ve on left side
+ *
+ ********************************/
+
+#include <stdio.h>
+
+#define ARR_SIZE 20
+
+struct Array {
+	int a[ARR_SIZE];
+	int size;
+	int length;
+};
+
+void disp(struct Array arr)
+{
+	int i;
+	for( i=0; i<arr.length; i++ )
+		printf("%d ", arr.a[i]);
+	printf("\n");
+}
+
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+void shiftNegative2left(struct Array *arr)
+{
+	int i, j;
+	i=0;
+	j = arr->length -1;
+
+	while(i<j)
+	{
+		while(arr->a[i] < 0) i++;
+		while(arr->a[j] >= 0) j--;
+		if(i<j) swap(&arr->a[i], &arr->a[j]);
+	}
+}
+
+int main()
+{
+	struct Array arr = { {2,-3,5,6,7,-1,-4,8}, 10,8};
+	disp(arr);
+
+	shiftNegative2left(&arr);
+	disp(arr);
+
+	return 0;
+}	
